9b1001ef205bd331798ad12d4d8f8598114ad909
alex <alex_khranovskiy@ukr.net>
2024-06-25 17:11:27 +0300

    Fixed spec.

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
-
-Route::get('categories', [CategoryController::class, 'getAll']);
-Route::get('categories/{id}', [CategoryController::class, 'show']);
-Route::post('categories', [CategoryController::class, 'create']);
-Route::put('categories/{id}', [CategoryController::class, 'update']);
-Route::delete('categories/{id}', [CategoryController::class, 'delete']);
-
-Route::get('products', [ProductController::class, 'getAll']);
-Route::get('products/{id}', [ProductController::class, 'show']);
-Route::post('products', [ProductController::class, 'create']);
-Route::delete('products/{id}', [ProductController::class, 'delete']);
-Route::post('products/{id}', [ProductController::class, 'update']);
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('signin', [AuthController::class, 'signIn']);
 Route::post('signup', [AuthController::class, 'signUp']);
@@ -29,5 +12,16 @@ Route::group(['middleware' => 'auth.jwt'], function () {
 
     Route::get('test', [AuthController::class, 'test']);
 
+    Route::get('categories', [CategoryController::class, 'getAll']);
+    Route::get('categories/{id}', [CategoryController::class, 'show']);
+    Route::post('categories', [CategoryController::class, 'create']);
+    Route::put('categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('categories/{id}', [CategoryController::class, 'delete']);
+
+    Route::get('products', [ProductController::class, 'getAll']);
+    Route::get('products/{id}', [ProductController::class, 'show']);
+    Route::post('products', [ProductController::class, 'create']);
+    Route::delete('products/{id}', [ProductController::class, 'delete']);
+    Route::post('products/{id}', [ProductController::class, 'update']);
 });
 
